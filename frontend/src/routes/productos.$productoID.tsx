aa7a215eedb2dd5fa62af83b7dcce25a3dc84538
Bartolome F. Rodriguez - Brot Mit Butter <spunit.br@gmail.com>
2026-04-27 22:09:35 -0300

    feat(productos/:id): Mostrar categorías

--- frontend/src/routes/productos.$productoID.tsx
+++ frontend/src/routes/productos.$productoID.tsx
@@ -37,15 +37,23 @@ function DetalleProducto() {
   if (error) return <div className="p-4 text-red-500">{error}</div>;
   if (!product) return <div className="p-4">Producto no encontrado.</div>;
 
+  let categoriesString = ">> ";
+
+  for (const category of product.categories) {
+    categoriesString += category.name + " - "
+  }
+  categoriesString = categoriesString.slice(0, -4);
+
   return (
-    <div className="p-6 flex flex-row">
+    <div className="p-6 flex flex-row gap-7">
       <img
         src={product.imgUrl || "https://placehold.co/400x200/e9d5ff/7e22ce?text=Producto"}
         alt={product.name}
         className="w-7x1 h-7x1 object-cover"
       />
-      <div className="flex flex-col self-stretch justify-between">
+      <div className="flex flex-col grow justify-between">
         <h2 className="text-3xl font-bold mb-2">{product.name}</h2>
+        <p className="text-0.5 mb-2 self-start"><u>{categoriesString}</u></p>
         <p className="text-xl text-blue-600 mb-4">Precio: ${product.price}</p>
 
         <div className="bg-gray-800 p-4 rounded-lg">
