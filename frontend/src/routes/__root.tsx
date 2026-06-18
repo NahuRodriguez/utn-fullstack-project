fb5ebe9a1aae63da29a790f443b16607b2da3fca
ceciliavlz <mariacvelzi@gmail.com>
2026-06-18 09:05:13 -0300

    feat: rediseña homepage con hero, footer y navbar

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -3,11 +3,13 @@ import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Navbar from "../components/navbar/Navbar";
 import { linkList } from "../assets/links";
 import { Header } from "../components/Header";
+import { Footer } from "../components/footer/Footer";
 const RootLayout = () => (
   <>
     <Header />
     <Navbar links={linkList} />
     <Outlet />
+    <Footer />
     <TanStackRouterDevtools />
   </>
 );
