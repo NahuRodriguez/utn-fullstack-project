1dcad5942a92539326c2a8e8d027a62deb93474e
ceciliavlz <mariacvelzi@gmail.com>
2026-06-18 08:03:23 -0300

    feat: elimina paginas innecesarias

--- frontend/src/routes/contacto.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/contacto")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/contacto"!</div>;
-}
